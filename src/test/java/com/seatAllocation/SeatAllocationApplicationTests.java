db7171e7d4bb92570475ad5154be67359d8d26de
강인혁 <ggh2260@gmail.com>
2023-04-22 23:33:07 +0900

    Navbar 생성

---
+++ src/test/java/com/seatAllocation/SeatAllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seatAllocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatAllocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
